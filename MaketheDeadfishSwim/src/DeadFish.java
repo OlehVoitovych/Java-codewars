31eae03e70a75dd42504dda1be1f36c72fe4fd0f
OlehVoitovych <oleg.voytovych04@gmail.com>
2023-10-24 15:50:10 +0300

    23

---
+++ MaketheDeadfishSwim/src/DeadFish.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class DeadFish {
+    private int[] res;
+
+    public DeadFish(String data) {
+        res = parse(data);
+    }
+
+    public static int[] parse(String data) {
+        int count = 0;
+        List<Integer> resList = new ArrayList<>();
+
+        int i = 0;
+        for (char c: data.toCharArray()) {
+            switch (c){
+                case 'i' -> {i++;}
+                case 'd' -> {i--;}
+                case 's' -> {i *= i;}
+                case 'o' -> {resList.add(i);}
+
+            }
+        }
+
+        return resList.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    @Override
+    public String toString() {
+        return "Deadfish{" +
+                "res=" + Arrays.toString(res) +
+                '}';
+    }
+}
---
+++ MaketheDeadfishSwim/src/Main.java
@@ -0,0 +1,19 @@
+/*Write a simple parser that will parse and run Deadfish.
+
+Deadfish has 4 commands, each 1 character long:
+
+i increments the value (initially 0)
+d decrements the value
+s squares the value
+o outputs the value into the return array
+Invalid characters should be ignored.
+
+*/
+
+public class Main {
+    public static void main(String[] args) {
+
+        DeadFish df = new DeadFish("iiisdoso");
+        System.out.println(df);
+    }
+}
